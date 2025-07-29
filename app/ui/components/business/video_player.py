d29479b48c53699848aad79d13e05ac33ab7e50f
ihmily <114978440+ihmily@users.noreply.github.com>
2025-07-29 10:38:31 +0800

    fix: use flet_video replaced

--- app/ui/components/business/video_player.py
+++ app/ui/components/business/video_player.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 
 import flet as ft
+import flet_video as ftv
 
 from ....utils import utils
 from ....utils.logger import logger
@@ -46,10 +47,10 @@ class VideoPlayer:
             video_width = 800
             video_height = 450
 
-        video = ft.Video(
+        video = ftv.Video(
             width=video_width,
             height=video_height,
-            playlist=[ft.VideoMedia(video_source)],
+            playlist=[ftv.VideoMedia(video_source)],
             autoplay=True
         )
 
@@ -142,4 +143,4 @@ class VideoPlayer:
                     return
             else:
                 title = self._["view_stream_source_now"]
-        await self.create_video_dialog(title, source, is_file_path, room_url) 
+        await self.create_video_dialog(title, source, is_file_path, room_url)
